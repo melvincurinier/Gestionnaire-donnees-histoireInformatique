ac2ded04aca4516685eb34113061ddbc927b3e36
Melvin Curinier <melvincurinier@gmail.com>
2024-02-08 11:32:30 +0100

    Tree click

--- src/views/HomeView.py
+++ src/views/HomeView.py
@@ -7,15 +7,36 @@ class HomeView(ctk.CTkFrame):
 
         self.app = app
 
-        columns = ('first_name', 'last_name', 'email')
+        columns = ('type', 'name', 'brief_description')
 
-        # Use self instead of parent for Treeview
         self.tree = ttk.Treeview(self, columns=columns, show='headings')
 
         # define headings
-        self.tree.heading('first_name', text='First Name')
-        self.tree.heading('last_name', text='Last Name')
-        self.tree.heading('email', text='Email')
+        self.tree.heading('type', text='Type')
+        self.tree.heading('name', text='Nom')
+        self.tree.heading('brief_description', text='Description briève')
+
+        self.tree.bind('<ButtonRelease-1>', self.on_tree_click)
 
         # Pack the Treeview widget
         self.tree.pack(expand=True, fill='both')
+
+        data = [
+            ('Personnalité', 'Tillian Dhume', 'Respo réseau Polytech'),
+            ('Personnalité', 'Will Smith', 'Acteur américain'),
+            # Ajoutez autant de lignes que nécessaire
+        ]
+
+        for item_data in data:
+            self.add_item(item_data)
+
+    def add_item(self, data):
+        # Ajoute une ligne avec les données spécifiées
+        self.tree.insert('', 'end', values=data)
+
+    def on_tree_click(self, event):
+        item = self.tree.selection()
+        
+        if item:
+            values = self.tree.item(item, 'values')
+            print("Row clicked:", values)
