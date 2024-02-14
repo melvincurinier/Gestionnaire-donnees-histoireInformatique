d7079c983a64dc05a61c8e6933a6322aff0b7230
Melvin Curinier <melvincurinier@gmail.com>
2024-02-14 18:24:31 +0100

    Dashboard finish

--- src/views/DashboardView.py
+++ src/views/DashboardView.py
@@ -10,15 +10,12 @@ class DashboardView(ctk.CTkFrame):
         self.app = app
         self.category = category
 
-        # Frame pour le titre et les boutons
         title_frame = ctk.CTkFrame(self)
         title_frame.pack(side='top', fill='x')
 
-        # Bouton "Retour"
         return_button = Button(title_frame, text='Retour', command=app.switch_to_searchData_view)
         return_button.grid(row=0, column=0, sticky='w')
 
-        # Titre principal
         title_label = Label(title_frame, text='Tableau d\'informations')
         title_label.grid(row=0, column=1, padx=10)
 
@@ -33,14 +30,25 @@ class DashboardView(ctk.CTkFrame):
             self.tree.heading('description', text='Description')
 
             self.tree.column('id',width=100)
+            self.tree.column('name',width=100)
+            self.tree.column('birthdate',width=100)
+            self.tree.column('deathdate',width=100)
+            self.tree.column('description',width=200)
         elif self.category == "Technologie":
-            columns = ('id', 'title', 'creationdate', 'description')
+            columns = ('id', 'title', 'type', 'creationdate', 'description')
             self.tree = ttk.Treeview(self, columns=columns, show='headings')
 
             self.tree.heading('id', text='Identifiant')
             self.tree.heading('title', text='Titre')
+            self.tree.heading('type', text='Type')
             self.tree.heading('creationdate', text='Année de création')
             self.tree.heading('description', text='Description')
+
+            self.tree.column('id',width=100)
+            self.tree.column('type',width=100)
+            self.tree.column('birthdate',width=100)
+            self.tree.column('creationdate',width=100)
+            self.tree.column('description',width=200)
         elif self.category == "Evénement":
             columns = ('id', 'title', 'date', 'location', 'description')
             self.tree = ttk.Treeview(self, columns=columns, show='headings')
@@ -50,16 +58,20 @@ class DashboardView(ctk.CTkFrame):
             self.tree.heading('date', text='Année')
             self.tree.heading('location', text='Lieu')
             self.tree.heading('description', text='Description')
+
+            self.tree.column('id',width=100)
+            self.tree.column('title',width=100)
+            self.tree.column('date',width=100)
+            self.tree.column('location',width=100)
+            self.tree.column('description',width=200)
         else:
             columns = ('id', 'nom', 'date', 'description')
             self.tree = ttk.Treeview(self, columns=columns, show='headings')
 
         self.tree.bind('<ButtonRelease-1>', self.on_tree_click)
 
-        # Pack the Treeview widget
         self.tree.pack(side='left', expand=False, fill='both')
 
-        # Frame à droite du tableau pour afficher les informations de l'item
         self.info_frame = ctk.CTkFrame(self)
         self.info_frame.pack(side='left', padx=10)
       
@@ -68,7 +80,6 @@ class DashboardView(ctk.CTkFrame):
                 self.add_item(item_data)
 
     def add_item(self, data):
-        # Ajoute une ligne avec les données spécifiées
         self.tree.insert('', 'end', values=data)
 
     def on_tree_click(self, event):
@@ -79,19 +90,48 @@ class DashboardView(ctk.CTkFrame):
         if item:
             values = self.tree.item(item, 'values')
 
-        # Efface les anciennes étiquettes
         for widget in self.info_frame.winfo_children():
             widget.destroy()
 
-        # Ajoute des étiquettes avec les informations de l'item
-        Label(self.info_frame, text='Type:').grid(row=0, column=0, sticky='e')
-        Label(self.info_frame, text=values[1]).grid(row=0, column=1)
+        if self.category == "Personnalité":
+            Label(self.info_frame, text='Nom:').grid(row=0, column=0, sticky='e')
+            Label(self.info_frame, text=values[1]).grid(row=0, column=1)
 
-        Label(self.info_frame, text='Nom:').grid(row=1, column=0, sticky='e')
-        Label(self.info_frame, text=values[2]).grid(row=1, column=1)
+            Label(self.info_frame, text='Année de naissance:').grid(row=1, column=0, sticky='e')
+            Label(self.info_frame, text=values[2]).grid(row=1, column=1)
 
-        Button(self.info_frame, text='Modifier', command=self.updateDataView_button_click).grid(row=3, column=0)
-        Button(self.info_frame, text='Supprimer', command=lambda: self.deleteData_button_click(values[0], item)).grid(row=3, column=1)
+            Label(self.info_frame, text='Année de décès:').grid(row=2, column=0, sticky='e')
+            Label(self.info_frame, text=values[3]).grid(row=2, column=1)
+
+            Label(self.info_frame, text='Description:').grid(row=3, column=0, sticky='e')
+            Label(self.info_frame, text=values[4]).grid(row=3, column=1)
+        elif self.category == "Technologie":
+            Label(self.info_frame, text='Title:').grid(row=0, column=0, sticky='e')
+            Label(self.info_frame, text=values[1]).grid(row=0, column=1)
+
+            Label(self.info_frame, text='Type:').grid(row=1, column=0, sticky='e')
+            Label(self.info_frame, text=values[4]).grid(row=1, column=1)
+
+            Label(self.info_frame, text='Année de création:').grid(row=2, column=0, sticky='e')
+            Label(self.info_frame, text=values[2]).grid(row=2, column=1)
+
+            Label(self.info_frame, text='Description:').grid(row=3, column=0, sticky='e')
+            Label(self.info_frame, text=values[3]).grid(row=3, column=1)
+        elif self.category == "Evénement":
+            Label(self.info_frame, text='Title:').grid(row=0, column=0, sticky='e')
+            Label(self.info_frame, text=values[1]).grid(row=0, column=1)
+
+            Label(self.info_frame, text='Année:').grid(row=1, column=0, sticky='e')
+            Label(self.info_frame, text=values[2]).grid(row=1, column=1)
+
+            Label(self.info_frame, text='Lieu:').grid(row=2, column=0, sticky='e')
+            Label(self.info_frame, text=values[3]).grid(row=2, column=1)
+
+            Label(self.info_frame, text='Description:').grid(row=3, column=0, sticky='e')
+            Label(self.info_frame, text=values[4], wraplength=250).grid(row=3, column=1)
+
+        Button(self.info_frame, text='Modifier', command=self.updateDataView_button_click).grid(row=4, column=1)
+        Button(self.info_frame, text='Supprimer', command=lambda: self.deleteData_button_click(values[0], item)).grid(row=5, column=1)
     
     def addDataView_button_click(self):
         self.app.switch_to_addData_view()
@@ -110,7 +150,6 @@ class DashboardView(ctk.CTkFrame):
         elif self.category == "Evénement":
             supprimer_un_evenement(id)
         self.tree.delete(item)
-        # Efface les anciennes étiquettes
         for widget in self.info_frame.winfo_children():
             widget.destroy()
     
